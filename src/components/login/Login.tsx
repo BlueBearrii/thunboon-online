4496232f69328539d13f125f685a7e6c0002cb06
bluebearrii <thananan.math.acs@gmail.com>
2021-11-18 15:57:55 +0700

    playground

--- src/components/login/Login.tsx
+++ src/components/login/Login.tsx
@@ -1,26 +1,67 @@
-import React, { FormEvent, ReactElement, useState } from "react";
+import React, { FormEvent, ReactElement, useEffect, useState } from "react";
+
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faTrashAlt } from "@fortawesome/free-solid-svg-icons";
 
 interface Props {}
 
 export default function Login({}: Props): ReactElement {
-  const [input, setInput] = useState<String>("");
+  const [input, setInput] = useState<string>("");
+  const [lists, setLists] = useState<Array<string>>([]);
+
+  useEffect(() => {
+    console.log(lists);
+  }, [lists]);
+
   const onSubmitHanler = (event: FormEvent) => {
     event.preventDefault();
-    if(input.length != 0) console.log("Hi :", input);
+    if (input.length != 0) {
+      console.log("Hi :", input);
+      setLists([...lists, input]);
+      setInput("");
+    }
+  };
+
+  const removeList = (index: number) => {
+    const task = [...lists];
+    task.splice(index, 1);
+    setLists(task);
   };
 
   return (
     <section style={loginContainer}>
       <div style={card}>
+        <p style={{ textAlign: "center", marginTop: "10px", color: "#666666" }}>
+          <b>TO DO</b>
+        </p>
         <form onSubmit={(e) => onSubmitHanler(e)} style={formContainer}>
           <input
             onChange={(e) => setInput(e.target.value)}
             placeholder="Enter something here"
+            value={input}
             style={inputForm}
           />
           <button style={btnSubmit}>Submit</button>
         </form>
       </div>
+      {lists.length == 0 ? null : (
+        <div
+          style={{ maxHeight: "50vh", padding: "0 10px", overflowY: "scroll" }}
+        >
+          {lists.map((item, index) => {
+            return (
+              <div style={card} key={index}>
+                {item}
+                <FontAwesomeIcon
+                  style={trash}
+                  icon={faTrashAlt}
+                  onClick={() => removeList(index)}
+                />
+              </div>
+            );
+          })}
+        </div>
+      )}
     </section>
   );
 }
@@ -29,7 +70,9 @@ const loginContainer = {
   display: "flex",
   justifyContent: "center",
   alignItems: "center",
+  flexDirection: "column" as "column",
   minHeight: "100vh",
+  height: "100%",
   background: "#D8D8E6",
 };
 
@@ -38,6 +81,13 @@ const card = {
   padding: "10px",
   borderRadius: "10px",
   minWidth: "300px",
+  marginBottom: "10px",
+};
+
+const trash = {
+  size: "0.5em",
+  float: "right" as "right",
+  color: "#D8D8E6"
 };
 
 const formContainer = {
